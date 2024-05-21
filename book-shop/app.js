786e943b37bccf06dbde133552b38787cb3b0421
bang-wol <milk011922@gmail.com>
2024-05-21 21:41:16 +0900

    Book Shop 프로젝트 app.js 구현
    - app.js 구현 업로드 과제

---
+++ book-shop/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config()
+
+app.listen(process.env.PORT);
+
+const userRouter=require('./routes/users');
+const bookRouter=require('./routes/books');
+const likeRouter=require('./routes/likes');
+const cartRouter=require('./routes/carts');
+const orederRouter=require('./routes/orders');
+
+app.use('/users',userRouter);
+app.use('/books',bookRouter);
+app.use('/likes',likeRouter);
+app.use('/cart',cartRouter);
+app.use('/order',orederRouter);
