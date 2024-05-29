e99d64dc21d6a42696cd50d02e574a9c867422b6
bang-wol <milk011922@gmail.com>
2024-05-29 12:50:01 +0900

    장바구니 기능 구현
    - 장바구니 담기, 목록 조회, 삭제 기능
    - 목록 조회 시 장바구니에서 선택한 아이템 목록 조회 가능하도록 구현

--- book-shop/app.js
+++ book-shop/app.js
@@ -7,12 +7,12 @@ app.listen(process.env.PORT);
 
 // const userRouter=require('./routes/users');
 const bookRouter=require('./routes/books');
-// const likeRouter=require('./routes/likes');
-// const cartRouter=require('./routes/carts');
+const likeRouter=require('./routes/likes');
+const cartRouter=require('./routes/carts');
 // const orederRouter=require('./routes/orders');
 
 // app.use('/users',userRouter);
 app.use('/books',bookRouter);
-// app.use('/likes',likeRouter);
-// app.use('/cart',cartRouter);
+app.use('/likes',likeRouter);
+app.use('/cart',cartRouter);
 // app.use('/order',orederRouter);
